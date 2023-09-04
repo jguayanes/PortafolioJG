7acfc7f51d354cfd1251a22d5d31e07f10c4fff3
jguayanes <julianguayanes4@gmail.com>
2023-09-03 21:39:15 -0500

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
         <div className="w-[140px]">
             <ul className="flex gap-2">
                 <li><a target="_blank" href="https://www.instagram.com/juliang_0302/"><img src="/insta.png" alt="" /></a></li>
-                <li><img src="/face.png" alt="" /></li>
-                <li><img src="/tw.png" alt="" /></li>
+                <li><a target="_blank" href="https://www.facebook.com/julianandres0304/"><img src="/face.png" alt="" /></a></li>
+                <li><a target="_blank" href="https://twitter.com/Julian92364861"><img src="/tw.png" alt="" /></a></li>
                 <li></li>
             </ul>
         </div>
